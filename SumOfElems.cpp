d82c6d3200f501f29d747b2f883f9e78dca17e3b
Alexander <pchp250g8@tutamail.com>
2025-12-23 14:45:02 +0300

    Fixed random values generator for array

--- SumOfElems.cpp
+++ SumOfElems.cpp
@@ -14,7 +14,7 @@ int _tmain(int argc, _TCHAR* argv[])
     srand(time(nullptr));
     for (int i = 0; i < nElems; i++)
     {
-        iArray[i] = 1 + rand() / 1000;
+        iArray[i] = 1 + rand() % 50;
     }
     std::cout << "Inputed array : ";
     for (int i = 0; i < nElems; i++)
